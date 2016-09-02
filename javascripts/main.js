ed9e48011705cc0351f154bb886ecfb2e717101b
Shravan Shetty <shravanshetty.ec@gmail.com>
2016-09-02 01:02:28 -0500

    Added google Maps API for location Indication

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,21 @@
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span>%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="pad">mobile</span><span>%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="pad">email</span><span>%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="pad">Mobile</span><span>%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="pad">Email</span><span>%data%</span></li>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a target="_blank" href="#">%data%';
 var HTMLschoolDegree = ' -- %data%';
 var HTMLschoolDates = ' (%data%)</a>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%';
+var HTMLworkDates = ' (%data%)</a>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p class="description"><br>%data%</p>';
 
 var ContactInfo = {
   "email": "shravanshetty@tamu.edu",
@@ -36,7 +43,7 @@ var education = {
         "dates": "2015-2017",
     "degree":"MS",
         "location": "College Station, TX",
-    "url":"https://www.tamu.edu/"
+    "url":"https://mays.tamu.edu/ms-management-information-systems/overview/"
     }, {
         "Name": "NMAMIT Nitte",
         "Majors": ["ECE"],
@@ -55,6 +62,30 @@ var education = {
   ]
 };
 
+var work = {
+    "jobs": [{
+        "employer": "Cisco",
+        "title": "Software Intern",
+        "location": "Dallas, TX",
+        "dates": "June 2016-Aug 2016",
+        "description": "Working as a full stack developer on Atlas and Spark  valuable technical and functional knowledge used in today’s fast passed industry. In terms of understanding Cisco’s business, I now have a good understanding of products like Atlas and Cisco Spark and its inner workings. Having worked with multiple teams, I now have an understanding of different layers of implementation and its interdependencies.<br> A few things I have achieved during this period are<br><br>\u2022     Improved UX by Redesigned PSTN order workflow for partner login in Atlas<br>\u2022     Deployed server side sort to eliminate error caused by pagination when dealing with bulk data<br>\u2022     Worked with a global team to implement order status feature in Cisco Spark, Helpdesk.<br>\u2022     Eliminated external Docker (Cassandra cluster) dependencies by implementing CasandraUnit."
+    }, {
+        "employer": "Texas A&M University",
+        "title": "Research Assistant",
+        "location": "College Station, TX",
+        "dates": "Aug 2015-May 2016",
+        "description": "Worked on developing an application that integrates multiple safety databases"
+    }, {
+        "employer": "Accenture",
+        "title": "Software Engineer",
+        "location": "Hyderabad, AP",
+        "dates": "Jan 2013-April 2015",
+        "description": "ERP Developer working on CRM"
+    }]
+};
+
+var googleMap = '<div id="map"></div>';
+
 if (bio.skills.length > 0){
   $("#skillSet").append(HTMLskillsStart);
   for (skill in bio.skills){
@@ -72,13 +103,35 @@ function displayEducation(){
     var formatedSchoolDegree = HTMLschoolDegree.replace("%data%",education.schools[school].degree);
     var formatedSchoolDate = HTMLschoolDates.replace("%data%",education.schools[school].dates);
     $("#education").append(formatedSchoolName+formatedSchoolDegree+formatedSchoolDate);
-    var formatedSchoolMajor = HTMLschoolMajor.replace("%data%",education.schools[school].Majors);
-    $("#education").append(formatedSchoolMajor);
     var formatedSchoolLocation = HTMLschoolLocation.replace("%data%",education.schools[school].location);
     $("#education").append(formatedSchoolLocation);
+    var formatedSchoolMajor = HTMLschoolMajor.replace("%data%",education.schools[school].Majors);
+    $("#education").append(formatedSchoolMajor);
     }
   }
 };
+
+displayEducation();
+
+function displayWork(){
+  if (work.jobs.length > 0){
+    for (job in work.jobs){
+    $("#workExperience").append(HTMLworkStart);
+    var formatedWorkEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+    var formatedJobTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+    var formatedWorkDate = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+    $("#workExperience").append(formatedWorkEmployer + formatedJobTitle + formatedWorkDate);
+
+    var formatedWorkLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+    $("#workExperience").append(formatedWorkLocation);
+
+    var formatedWorkDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+    $("#workExperience").append(formatedWorkDescription);
+    }
+  }
+};
+
+displayWork();
 
 formatedEmail = HTMLemail.replace("%data%",bio.ContactInformation.email)
 $("#contact").append(formatedEmail);
@@ -86,4 +139,153 @@ $("#contact").append(formatedEmail);
 formatedMobile = HTMLmobile.replace("%data%",bio.ContactInformation.mobile)
 $("#contact").append(formatedMobile);
 
-displayEducation();
+//GoogleMap Implementation
+var map;
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    disableDefaultUI: true
+  };
+  /*
+  For the map to be displayed, the googleMap var must be
+  appended to #mapDiv.
+  */
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // adds the single location property from bio to the locations array
+    locations.push(bio.ContactInformation.location);
+
+    // iterates through school locations and appends each location to
+    // the locations array. Note that forEach is used for array iteration
+    // as described in the Udacity FEND Style Guide:
+    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
+
+    // iterates through work locations and appends each location to
+    // the locations array. Note that forEach is used for array iteration
+    // as described in the Udacity FEND Style Guide:
+    // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
+    work.jobs.forEach(function(job){
+      locations.push(job.location);
+    });
+    console.log(locations);
+    locations.shift();
+
+    return locations;
+  }
+
+  /*
+  createMapMarker(placeData) reads Google Places search results to create map pins.
+  placeData is the object returned from search results containing information
+  about a single location.
+  */
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+    // hmmmm, I wonder what this is about...
+    google.maps.event.addListener(marker, 'click', function() {
+      // your code goes here!
+    });
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+      locations.forEach(function(place){
+      // the search request object
+      console.log('Name of place:', place)
+      var request = {
+        query: place
+      };
+
+      // Actually searches the Google Maps API for location data and runs the callback
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    });
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+/*
+Uncomment the code below when you're ready to implement a Google Map!
+*/
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+  //Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
+
+$("#mapDiv").append(googleMap);
